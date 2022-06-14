0c8f91c5647f7f540295755e6a966cc17b797d21
BearMauler <bearmauler88@gmail.com>
2022-06-14 15:54:49 -0500

    created CBBSetup

---
+++ test/convertibleBondBox/CBBSetFee.t.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../../src/contracts/ConvertibleBondBox.sol";
+import "../../src/contracts/CBBFactory.sol";
+import "../../src/contracts/ButtonWoodBondController.sol";
+import "@buttonwood-protocol/tranche/contracts/interfaces/ITranche.sol";
+import "@buttonwood-protocol/tranche/contracts/Tranche.sol";
+import "@buttonwood-protocol/tranche/contracts/TrancheFactory.sol";
+import "../../src/contracts/CBBSlip.sol";
+import "../../src/contracts/CBBSlipFactory.sol";
+import "forge-std/console2.sol";
+import "../../test/mocks/MockERC20.sol";
+import "./CBBSetup.sol";
+
+contract CBBSetFee is CBBSetup {
+
+    function testSetFee(uint newFee) public {
+        console2.log(s_ratios[0], "s_ratios");
+        console.log(newFee, "newFee");
+        newFee = bound(newFee, 0, s_maxFeeBPS);
+        console.log(s_deployedConvertibleBondBox.owner(), "s_deployedConvertibleBondBox.owner()");
+        vm.prank(s_deployedConvertibleBondBox.owner());
+        vm.expectEmit(true, true, true, true);
+        emit FeeUpdate(newFee);
+        s_deployedConvertibleBondBox.setFee(newFee);
+        assertEq(s_deployedConvertibleBondBox.feeBps(), newFee);
+    }
+
+        function testFailSetFeeCalledByNonOwner(uint newFee) public {
+        newFee = bound(newFee, 0, s_BPS);
+        s_deployedConvertibleBondBox.setFee(newFee);
+    }
+
+    function testCannotSetFeeBondIsMature(uint newFee) public {
+        s_buttonWoodBondController.mature();
+        newFee = bound(newFee, 0, s_BPS);
+        vm.prank(s_deployedConvertibleBondBox.owner());
+        bytes memory customError = abi.encodeWithSignature(
+            "BondIsMature(bool,bool)",
+            true,
+            false
+        );
+        vm.expectRevert(customError);
+        s_deployedConvertibleBondBox.setFee(newFee);
+    }
+
+    function testCannotSetFeeFeeTooLarge(uint newFee) public {
+        newFee = bound(newFee, s_BPS, type(uint256).max);
+        vm.prank(s_deployedConvertibleBondBox.owner());
+        bytes memory customError = abi.encodeWithSignature(
+            "FeeTooLarge(uint256,uint256)",
+            newFee,
+            s_maxFeeBPS
+        );
+        vm.expectRevert(customError);
+        s_deployedConvertibleBondBox.setFee(newFee);
+    }
+}
--- test/convertibleBondBox/ConvertibleBondBox.t.sol
+++ test/convertibleBondBox/ConvertibleBondBox.t.sol
@@ -2,138 +2,19 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/contracts/ConvertibleBondBox.sol";
-import "../src/contracts/CBBFactory.sol";
-import "../src/contracts/ButtonWoodBondController.sol";
+import "../../src/contracts/ConvertibleBondBox.sol";
+import "../../src/contracts/CBBFactory.sol";
+import "../../src/contracts/ButtonWoodBondController.sol";
 import "@buttonwood-protocol/tranche/contracts/interfaces/ITranche.sol";
 import "@buttonwood-protocol/tranche/contracts/Tranche.sol";
 import "@buttonwood-protocol/tranche/contracts/TrancheFactory.sol";
-import "../src/contracts/CBBSlip.sol";
-import "../src/contracts/CBBSlipFactory.sol";
+import "../../src/contracts/CBBSlip.sol";
+import "../../src/contracts/CBBSlipFactory.sol";
 import "forge-std/console2.sol";
-import "../test/mocks/MockERC20.sol";
+import "../../test/mocks/MockERC20.sol";
+import "./CBBSetup.sol";
 
-contract ConvertibleBondBoxTest is Test {
-    ButtonWoodBondController s_buttonWoodBondController;
-    ConvertibleBondBox s_convertibleBondBox;
-    ConvertibleBondBox s_deployedConvertibleBondBox;
-    CBBFactory s_CBBFactory;
-
-    MockERC20 s_collateralToken;
-
-    MockERC20 s_stableToken;
-    TrancheFactory s_trancheFactory;
-    Tranche s_tranche;
-    CBBSlip s_slip;
-    CBBSlipFactory s_slipFactory;
-    ITranche s_safeTranche;
-    ITranche s_riskTranche;
-    uint256[] s_ratios;
-    uint256 s_depositLimit;
-    uint256 s_safeRatio;
-    uint256 s_riskRatio;
-    uint256 constant s_penalty = 500;
-    uint256 constant s_price = 5e8;
-    uint256 constant s_trancheIndex = 0;
-    uint256 constant s_maturityDate = 1656717949;
-    uint256 constant s_safeSlipAmount = 10;
-    uint256 constant s_endOfUnixTime = 2147483647;
-    uint256 constant s_trancheGranularity = 1000;
-    uint256 constant s_penaltyGranularity = 1000;
-    uint256 constant s_priceGranularity = 1e9;
-    uint256 constant s_BPS = 10_000;
-    uint256 private constant s_maxFeeBPS = 50;
-    address s_deployedCBBAddress;
-
-    event ConvertibleBondBoxCreated(
-        address s_collateralToken,
-        address s_stableToken,
-        uint256 trancheIndex,
-        uint256 penalty,
-        address creator
-    );
-
-    event Lend(address, address, address, uint256, uint256);
-    event Borrow(address, address, address, uint256, uint256);
-    event RedeemStable(address, uint256, uint256);
-    event RedeemSafeTranche(address, uint256);
-    event RedeemRiskTranche(address, uint256);
-    event Repay(address, uint256, uint256, uint256);
-    event Initialized(address, address, uint256, uint256);
-    event FeeUpdate(uint256);
-
-    function setUp() public {
-        //push numbers into array
-        s_ratios.push(200);
-        s_ratios.push(300);
-        s_ratios.push(500);
-
-        // create buttonwood bond collateral token
-        s_collateralToken = new MockERC20("CollateralToken", "CT");
-        s_collateralToken.mint(address(this), 1e18);
-
-        // // create stable token
-        s_stableToken = new MockERC20("StableToken", "ST");
-        s_stableToken.mint(address(this), 10e18);
-        // // create tranche
-        s_tranche = new Tranche();
-
-        // // create buttonwood tranche factory
-        s_trancheFactory = new TrancheFactory(address(s_tranche));
-
-        // // create s_slip
-        s_slip = new CBBSlip();
-
-        // // create s_slip factory
-        s_slipFactory = new CBBSlipFactory(address(s_slip));
-
-        s_buttonWoodBondController = new ButtonWoodBondController();
-        s_convertibleBondBox = new ConvertibleBondBox();
-        s_CBBFactory = new CBBFactory(address(s_convertibleBondBox));
-
-        s_buttonWoodBondController.init(
-            address(s_trancheFactory),
-            address(s_collateralToken),
-            address(this),
-            s_ratios,
-            s_maturityDate,
-            s_depositLimit
-        );
-
-        s_deployedCBBAddress = s_CBBFactory.createConvertibleBondBox(
-            s_buttonWoodBondController,
-            s_slipFactory,
-            s_penalty,
-            address(s_collateralToken),
-            address(s_stableToken),
-            s_price,
-            s_trancheIndex
-        );
-
-        s_collateralToken.approve(
-            address(s_buttonWoodBondController),
-            type(uint256).max
-        );
-
-        s_buttonWoodBondController.deposit(1e18);
-
-        (s_safeTranche, s_safeRatio) = s_buttonWoodBondController.tranches(
-            s_trancheIndex
-        );
-        (s_riskTranche, s_riskRatio) = s_buttonWoodBondController.tranches(
-            s_buttonWoodBondController.trancheCount() - 1
-        );
-
-        s_safeTranche.approve(s_deployedCBBAddress, type(uint256).max);
-        s_riskTranche.approve(s_deployedCBBAddress, type(uint256).max);
-        s_stableToken.approve(s_deployedCBBAddress, type(uint256).max);
-
-        s_deployedConvertibleBondBox = ConvertibleBondBox(s_deployedCBBAddress);
-
-        s_depositLimit =
-            (s_safeTranche.balanceOf(address(this)) * s_price) /
-            s_priceGranularity;
-    }
+contract ConvertibleBondBoxTest is CBBSetup {
 
     // initialize()
     function testInitializeOwnerTransfer(uint256 collateralAmount)
@@ -1165,45 +1046,6 @@ contract ConvertibleBondBoxTest is Test {
         );
         vm.expectRevert(customError);
         s_deployedConvertibleBondBox.redeemStable(s_safeSlipAmount);
-    }
-
-    function testSetFee(uint newFee) public {
-        newFee = bound(newFee, 0, s_maxFeeBPS);
-        vm.prank(s_deployedConvertibleBondBox.owner());
-        vm.expectEmit(true, true, true, true);
-        emit FeeUpdate(newFee);
-        s_deployedConvertibleBondBox.setFee(newFee);
-        assertEq(s_deployedConvertibleBondBox.feeBps(), newFee);
-    }
-
-        function testFailSetFeeCalledByNonOwner(uint newFee) public {
-        newFee = bound(newFee, 0, s_BPS);
-        s_deployedConvertibleBondBox.setFee(newFee);
-    }
-
-    function testCannotSetFeeBondIsMature(uint newFee) public {
-        s_buttonWoodBondController.mature();
-        newFee = bound(newFee, 0, s_BPS);
-        vm.prank(s_deployedConvertibleBondBox.owner());
-        bytes memory customError = abi.encodeWithSignature(
-            "BondIsMature(bool,bool)",
-            true,
-            false
-        );
-        vm.expectRevert(customError);
-        s_deployedConvertibleBondBox.setFee(newFee);
-    }
-
-    function testCannotSetFeeFeeTooLarge(uint newFee) public {
-        newFee = bound(newFee, s_BPS, type(uint256).max);
-        vm.prank(s_deployedConvertibleBondBox.owner());
-        bytes memory customError = abi.encodeWithSignature(
-            "FeeTooLarge(uint256,uint256)",
-            newFee,
-            s_maxFeeBPS
-        );
-        vm.expectRevert(customError);
-        s_deployedConvertibleBondBox.setFee(newFee);
     }
 
     function testEndToEnd(
