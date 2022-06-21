12ac16079bd6fde29916bd4cfa82ee301621f11a
sunbreather <hinokami_kagura0@protonmail.com>
2022-06-21 16:54:31 -0500

    wip

---
+++ test/stagingBox/TransmitReinit.t.sol
@@ -0,0 +1,40 @@
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+import "../../src/contracts/StagingBox.sol";
+import "../../src/contracts/StagingBoxFactory.sol";
+import "../../src/contracts/CBBFactory.sol";
+import "../../src/contracts/ConvertibleBondBox.sol";
+import "./SBSetup.t.sol";
+
+contract TransmitReinit is SBSetup {
+
+    function testTransmitReInit(uint256 price, uint256 _borrowSlipAmount, bool _isLend) public {
+        price = bound(price, 0, s_deployedConvertibleBondBox.s_priceGranularity());
+
+        s_deployedSB = StagingBox(stagingBoxFactory.createStagingBox(
+            s_deployedConvertibleBondBox,
+            s_slipFactory,
+            price,
+            s_cbb_owner
+        ));
+
+        vm.startPrank(s_cbb_owner);
+
+        // TODO: can we mock these?
+        vm.mockCall(
+            address(s_deployedConvertibleBondBox), 
+            abi.encodeWithSelector(s_deployedConvertibleBondBox.reinitialize.selector),
+            abi.encode(true)
+        );
+
+        vm.mockCall(
+            address(s_deployedConvertibleBondBox), 
+            abi.encodeWithSelector(s_deployedConvertibleBondBox.cbbTransferOwnership.selector),
+            abi.encode(true)
+        );
+
+        s_deployedSB.transmitReInit(_isLend);
+        vm.stopPrank();
+    }
+}
