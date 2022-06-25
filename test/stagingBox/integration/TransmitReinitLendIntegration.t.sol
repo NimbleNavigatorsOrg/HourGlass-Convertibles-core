0a2d388175bd818412b840af33581a79c6388360
BearMauler <bearmauler88@gmail.com>
2022-06-25 06:00:16 -0500

    wip

--- test/stagingBox/integration/TransmitReinitLendIntegration.t.sol
+++ test/stagingBox/integration/TransmitReinitLendIntegration.t.sol
@@ -12,34 +12,10 @@ contract TransmitReinitLendIntegration is SBIntegrationSetup {
     function testTransmitReinitIntegrationIsLendTrueEmitsLend(uint256 fuzzPrice) public {
         transmitReinitIntegrationSetup(fuzzPrice, true);
 
-        uint256 sbStableTokenBalanceBeforeLend = IERC20(s_stableToken).balanceOf(address(s_deployedSB));
-        uint256 sbRiskTrancheBalanceBefore = s_riskTranche.balanceOf(address(s_deployedSB));
-
         vm.prank(s_cbb_owner);
         vm.expectEmit(true, true, true, true);
         emit Lend(address(s_deployedSB), address(s_deployedSB), address(s_deployedSB), maxStableAmount, s_price);
         s_deployedSB.transmitReInit(s_isLend);
-
-        uint256 sbRiskTrancheBalanceAfter = s_riskTranche.balanceOf(address(s_deployedSB));
-
-        uint256 borrowerStableBalanceAfter = s_stableToken.balanceOf(address(s_deployedSB));
-        uint256 borrowerRiskSlipsAfter = ISlip(s_deployedConvertibleBondBox.s_riskSlipTokenAddress()).balanceOf(address(s_deployedSB));
-
-        uint256 lenderSafeSlipsAfter = ISlip(
-            s_deployedConvertibleBondBox.s_safeSlipTokenAddress()
-        ).balanceOf(address(s_deployedSB));
-
-        uint256 mintAmount = (sbStableTokenBalanceBeforeLend * s_priceGranularity) /
-            s_deployedConvertibleBondBox.currentPrice();
-        uint256 expectedZ = (mintAmount * s_ratios[2]) / s_ratios[0];
-
-        assertEq(
-            sbRiskTrancheBalanceBefore - expectedZ,
-            sbRiskTrancheBalanceAfter
-        );
-        assertEq(borrowerStableBalanceAfter, sbStableTokenBalanceBeforeLend);
-        assertEq(borrowerRiskSlipsAfter, expectedZ);
-        assertEq(lenderSafeSlipsAfter, mintAmount);
     }
 
     function testTransmitReinitIntegrationIsLendTrueTransfersSafeTranchesFromSBToCBB(uint256 fuzzPrice) public {
@@ -94,5 +70,59 @@ contract TransmitReinitLendIntegration is SBIntegrationSetup {
             cbbRiskTrancheBalanceBefore + expectedZ, 
             cbbRiskTrancheBalanceAfter
         );
+    }
+
+    function testTransmitReinitIntegrationIsLendTrueMintsSafeSlipsToStagingBox(uint256 fuzzPrice) public {
+        transmitReinitIntegrationSetup(fuzzPrice, true);
+
+        uint256 sbStableTokenBalanceBeforeLend = IERC20(s_stableToken).balanceOf(address(s_deployedSB));
+
+        uint256 sbSafeSlipBalanceBefore = ISlip(
+            s_deployedConvertibleBondBox.s_safeSlipTokenAddress()
+        ).balanceOf(address(s_deployedSB));
+
+        vm.prank(s_cbb_owner);
+        s_deployedSB.transmitReInit(s_isLend);
+
+        uint256 sbSafeSlipBalanceAfter = ISlip(
+            s_deployedConvertibleBondBox.s_safeSlipTokenAddress()
+        ).balanceOf(address(s_deployedSB));
+
+        uint256 mintAmount = (sbStableTokenBalanceBeforeLend * s_priceGranularity) /
+            s_deployedConvertibleBondBox.currentPrice();
+
+        assertEq(sbSafeSlipBalanceBefore + mintAmount, sbSafeSlipBalanceAfter);
+    }
+
+    function testTransmitReinitIntegrationIsLendMintsRiskSlipsToStagingBox(uint256 fuzzPrice) public {
+        transmitReinitIntegrationSetup(fuzzPrice, true);
+
+        uint256 sbStableTokenBalanceBeforeLend = IERC20(s_stableToken).balanceOf(address(s_deployedSB));
+
+        uint256 sbRiskSlipBalanceBefore = ISlip(s_deployedConvertibleBondBox.s_riskSlipTokenAddress()).balanceOf(address(s_deployedSB));
+
+        vm.prank(s_cbb_owner);
+        s_deployedSB.transmitReInit(s_isLend);
+
+        uint256 sbRiskSlipBalanceAfter = ISlip(s_deployedConvertibleBondBox.s_riskSlipTokenAddress()).balanceOf(address(s_deployedSB));
+
+        uint256 mintAmount = (sbStableTokenBalanceBeforeLend * s_priceGranularity) /
+            s_deployedConvertibleBondBox.currentPrice();
+        uint256 expectedZ = (mintAmount * s_ratios[2]) / s_ratios[0];
+
+        assertEq(sbRiskSlipBalanceBefore + expectedZ, sbRiskSlipBalanceAfter);
+    }
+
+    function testTransmitReinitIntegrationIsLendTrueTransfersStablesFromSBToSB(uint256 fuzzPrice) public {
+        transmitReinitIntegrationSetup(fuzzPrice, true);
+
+        uint256 sbStableBalanceBefore = IERC20(s_stableToken).balanceOf(address(s_deployedSB));
+
+        vm.prank(s_cbb_owner);
+        s_deployedSB.transmitReInit(s_isLend);
+
+        uint256 sbStableBalanceAfter = s_stableToken.balanceOf(address(s_deployedSB));
+
+        assertEq(sbStableBalanceBefore, sbStableBalanceAfter);
     }
 }
