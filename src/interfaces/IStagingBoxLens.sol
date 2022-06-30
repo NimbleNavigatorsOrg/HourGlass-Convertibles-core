1c25ec52c22fcdaddb08d29a5b82c2a140d97570
G-Yes95 <g-yes95@protonmail.com>
2022-06-29 18:28:27 -0700

    Segregated into Lens Contract

---
+++ src/interfaces/IStagingBoxLens.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+import "../interfaces/IStagingBox.sol";
+import "../interfaces/IConvertibleBondBox.sol";
+import "../interfaces/IButtonWoodBondController.sol";
+
+interface IStagingBoxLens {
+    function viewSimpleWrapTrancheBorrow(
+        IStagingBox _stagingBox,
+        uint256 _amountRaw
+    ) external view returns (uint256, uint256);
+
+    /**
+     * @dev provides amount of stableTokens expected in return for redeeming lendSlips
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * @param _lendSlipAmount The amount of lendSlips to be redeemed
+     * Requirements:
+     */
+
+    function viewRedeemLendSlipsForStables(
+        IStagingBox _stagingBox,
+        uint256 _lendSlipAmount
+    ) external view returns (uint256);
+
+    /**
+     * @dev provides amount of unwrapped collateral tokens expected in return for redeeming lendSlips
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * @param _lendSlipAmount The amount of lendSlips to be redeemed
+     * Requirements:
+     */
+
+    function viewRedeemLendSlipsForTranches(
+        IStagingBox _stagingBox,
+        uint256 _lendSlipAmount
+    ) external view returns (uint256, uint256);
+
+    /**
+     * @dev provides amount of unwrapped collateral tokens expected in return for redeeming riskSlips
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * @param _riskSlipAmount The amount of riskSlips to be redeemed
+     * Requirements:
+     */
+
+    function viewRedeemRiskSlipsForTranches(
+        IStagingBox _stagingBox,
+        uint256 _riskSlipAmount
+    ) external view returns (uint256, uint256);
+
+    /**
+     * @dev provides amount of unwrapped collateral tokens expected in return for repaying a specified amount of stables
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * @param _stableAmount The amount of stables being repaid
+     * Requirements:
+     *      - Only for prior to maturity
+     *      - Only for bonds with A/Z tranches
+     */
+
+    function viewRepayAndUnwrapSimple(
+        IStagingBox _stagingBox,
+        uint256 _stableAmount
+    ) external view returns (uint256, uint256);
+
+    /**
+     * @dev provides amount of unwrapped collateral tokens expected in return for repaying a specified amount of stables
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * @param _stableAmount The amount of stables being repaid
+     * Requirements:
+     *      - Only for after maturity
+     
+     */
+
+    function viewRepayAndUnwrapMature(
+        IStagingBox _stagingBox,
+        uint256 _stableAmount
+    ) external view returns (uint256, uint256);
+
+    /**
+     * @dev provides amount of unwrapped collateral tokens expected in return for repaying all of the user's RiskSlips
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * Requirements:
+     *      - Only for prior to maturity
+     */
+
+    function viewRepayAndUnwrapMax(IStagingBox _stagingBox)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+
+    /**
+     * @dev provides amount of unwrapped collateral tokens expected in return for repaying all of the user's RiskSlips
+     * @param _stagingBox The staging box tied to the Convertible Bond
+     * Requirements:
+     *      - Only for after maturity
+     */
+
+    function viewRepayAndUnwrapMaxMature(IStagingBox _stagingBox)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+}
