01d592f3fe4def0261180d70acc6c59c9eb6849e
BearMauler <bearmauler88@gmail.com>
2022-06-30 11:37:18 -0500

    fixed DepositBorrow tests.

--- test/stagingBox/DepositBorrow.t.sol
+++ test/stagingBox/DepositBorrow.t.sol
@@ -16,7 +16,7 @@ contract DepositBorrow is SBIntegrationSetup {
         uint256 sbSafeTrancheBalanceBeforeDeposit = s_deployedConvertibleBondBox.safeTranche().balanceOf(address(s_deployedSB));
         uint256 userSafeTrancheBalanceBeforeDeposit = s_deployedConvertibleBondBox.safeTranche().balanceOf(s_user);
 
-        _safeTrancheAmount = bound(_safeTrancheAmount, 0, userSafeTrancheBalanceBeforeDeposit);
+        _safeTrancheAmount = bound(_safeTrancheAmount, 1, userSafeTrancheBalanceBeforeDeposit);
 
         vm.prank(s_user);
         s_deployedSB.depositBorrow(s_borrower, _safeTrancheAmount);
@@ -26,6 +26,8 @@ contract DepositBorrow is SBIntegrationSetup {
 
         assertEq(userSafeTrancheBalanceBeforeDeposit - _safeTrancheAmount, userSafeTrancheBalanceAfterDeposit);
         assertEq(sbSafeTrancheBalanceBeforeDeposit + _safeTrancheAmount, sbSafeTrancheBalanceAfterDeposit);
+
+        assertFalse(_safeTrancheAmount == 0);
     }
 
     function testSendsRiskTranchesFromMsgSenderToStagingBox(uint256 _fuzzPrice, uint256 _safeTrancheAmount) public {
@@ -37,7 +39,7 @@ contract DepositBorrow is SBIntegrationSetup {
         uint256 userRiskTrancheBalanceBeforeDeposit = s_deployedConvertibleBondBox.riskTranche().balanceOf(s_user);
         uint256 sbRiskTrancheBalanceBeforeDeposit = s_deployedConvertibleBondBox.riskTranche().balanceOf(address(s_deployedSB));
 
-        _safeTrancheAmount = bound(_safeTrancheAmount, 0, userSafeTrancheBalanceBeforeDeposit);
+        _safeTrancheAmount = bound(_safeTrancheAmount, s_deployedConvertibleBondBox.safeRatio(), userSafeTrancheBalanceBeforeDeposit);
 
         uint256 riskTrancheAmount = (_safeTrancheAmount * s_deployedConvertibleBondBox.riskRatio()) / s_deployedConvertibleBondBox.safeRatio();
 
@@ -52,6 +54,8 @@ contract DepositBorrow is SBIntegrationSetup {
 
         assertEq(userRiskTrancheBalanceBeforeDeposit - riskTrancheAmount, userRiskTrancheBalanceAfterDeposit);
         assertEq(sbRiskTrancheBalanceBeforeDeposit + riskTrancheAmount, sbRiskTrancheBalanceAfterDeposit);
+
+        assertFalse(riskTrancheAmount == 0);
     }
 
     function testMintsBorrowSlipsToBorrower(uint256 _fuzzPrice, uint256 _safeTrancheAmount) public {
@@ -61,7 +65,7 @@ contract DepositBorrow is SBIntegrationSetup {
         uint256 userSafeTrancheBalanceBeforeDeposit = s_deployedConvertibleBondBox.safeTranche().balanceOf(s_user);
         uint256 borrowerBorrowSlipBalanceBeforeDeposit = ISlip(s_deployedSB.s_borrowSlipTokenAddress()).balanceOf(s_borrower);
 
-        _safeTrancheAmount = bound(_safeTrancheAmount, 0, userSafeTrancheBalanceBeforeDeposit);
+        _safeTrancheAmount = bound(_safeTrancheAmount, 1, userSafeTrancheBalanceBeforeDeposit);
 
         ITranche(s_deployedConvertibleBondBox.safeTranche()).approve(address(s_deployedSB), type(uint256).max);
         ITranche(s_deployedConvertibleBondBox.riskTranche()).approve(address(s_deployedSB), type(uint256).max);
@@ -72,6 +76,8 @@ contract DepositBorrow is SBIntegrationSetup {
         uint256 borrowerBorrowSlipBalanceAfterDeposit = ISlip(s_deployedSB.s_borrowSlipTokenAddress()).balanceOf(s_borrower);
 
         assertEq(borrowerBorrowSlipBalanceBeforeDeposit + _safeTrancheAmount, borrowerBorrowSlipBalanceAfterDeposit);
+
+        assertFalse(_safeTrancheAmount == 0);
     }
 
     function testEmitsBorrowDeposit(uint256 _fuzzPrice, uint256 _safeTrancheAmount) public {
@@ -80,7 +86,7 @@ contract DepositBorrow is SBIntegrationSetup {
         
         uint256 userSafeTrancheBalanceBeforeDeposit = s_deployedConvertibleBondBox.safeTranche().balanceOf(s_user);
 
-        _safeTrancheAmount = bound(_safeTrancheAmount, 0, userSafeTrancheBalanceBeforeDeposit);
+        _safeTrancheAmount = bound(_safeTrancheAmount, 1, userSafeTrancheBalanceBeforeDeposit);
 
         ITranche(s_deployedConvertibleBondBox.safeTranche()).approve(address(s_deployedSB), type(uint256).max);
         ITranche(s_deployedConvertibleBondBox.riskTranche()).approve(address(s_deployedSB), type(uint256).max);
@@ -89,5 +95,7 @@ contract DepositBorrow is SBIntegrationSetup {
         vm.expectEmit(true, false, false, false);
         emit BorrowDeposit(s_borrower, _safeTrancheAmount);
         s_deployedSB.depositBorrow(s_borrower, _safeTrancheAmount);
+
+        assertFalse(_safeTrancheAmount == 0);
     }
 }
