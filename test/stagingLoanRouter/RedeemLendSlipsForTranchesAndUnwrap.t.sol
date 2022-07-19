1913c6885e1d0e971a1f71902a2126d4617624c9
BearMauler <bearmauler88@gmail.com>
2022-07-19 13:18:28 -0500

    improved assert

--- test/stagingLoanRouter/RedeemLendSlipsForTranchesAndUnwrap.t.sol
+++ test/stagingLoanRouter/RedeemLendSlipsForTranchesAndUnwrap.t.sol
@@ -49,8 +49,6 @@ contract RedeemLendSlipsForTranchesAndUnwrap is RedeemLendSlipsForStablesTestSet
 
         (uint256 underlyingAmount, uint256 buttonAmount) = StagingBoxLens(s_stagingBoxLens).viewRedeemLendSlipsForTranches(s_deployedSB, lendSlipAmount);
 
-        uint256 safeSlipBurnAmount = (lendSlipAmount * s_deployedSB.priceGranularity()) / s_deployedSB.initialPrice();
-
         uint256 sbSafeSlipBalanceBefore = ISlip(s_deployedSB.safeSlipAddress()).balanceOf(address(s_deployedSB));
         uint256 routerSafeSlipBalanceBefore = ISlip(s_deployedSB.safeSlipAddress()).balanceOf(address(s_stagingLoanRouter));
         uint256 cbbSafeSlipBalanceBefore = ISlip(s_deployedSB.safeSlipAddress()).balanceOf(address(s_deployedConvertibleBondBox));
@@ -62,12 +60,12 @@ contract RedeemLendSlipsForTranchesAndUnwrap is RedeemLendSlipsForStablesTestSet
         uint256 routerSafeSlipBalanceAfter = ISlip(s_deployedSB.safeSlipAddress()).balanceOf(address(s_stagingLoanRouter));
         uint256 cbbSafeSlipBalanceAfter = ISlip(s_deployedSB.safeSlipAddress()).balanceOf(address(s_deployedConvertibleBondBox));
 
-        assertEq(sbSafeSlipBalanceBefore - safeSlipBurnAmount, sbSafeSlipBalanceAfter);
+        assertEq(sbSafeSlipBalanceBefore - buttonAmount, sbSafeSlipBalanceAfter);
         assertEq(routerSafeSlipBalanceBefore, routerSafeSlipBalanceAfter);
         assertEq(cbbSafeSlipBalanceBefore, cbbSafeSlipBalanceAfter);
 
         assertFalse(lendSlipAmount == 0);
-        assertFalse(safeSlipBurnAmount == 0);
+        assertFalse(buttonAmount == 0);
     }
 
     function testRedeemLendSlipsForTranchesReturnsUnderlyingToMsgSender(uint256 _fuzzPrice, uint256 _lendAmount, uint256 _timeWarp, uint256 _lendSlipAmount) public {
