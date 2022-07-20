f0e9f37fb6c73b3bb5e0aa6c39877dde09b76ccd
BearMauler <bearmauler88@gmail.com>
2022-07-20 12:24:37 -0500

    wrote final test for RedeemRiskSlipsForTranchesAndUnwrap.

--- test/stagingLoanRouter/RedeemRiskSlipsForTranchesAndUnwrap.t.sol
+++ test/stagingLoanRouter/RedeemRiskSlipsForTranchesAndUnwrap.t.sol
@@ -34,7 +34,7 @@ contract RedeemRiskSlipsForTranchesAndUnwrap is RedeemLendSlipsForStablesTestSet
         assertEq(borrowerRiskSlipBalanceBefore - riskSlipRedeemAmount, borrowerRiskSlipBalanceAfter);
     }
 
-        function testRedeemRiskSlipsForTranchesAndUnwrapSendsUnderlyingToMsgSender(uint256 _fuzzPrice, uint256 _lendAmount) public {
+    function testRedeemRiskSlipsForTranchesAndUnwrapSendsUnderlyingToMsgSender(uint256 _fuzzPrice, uint256 _lendAmount) public {
         setupStagingBox(_fuzzPrice);
         setupTranches(false, s_owner, s_deployedCBBAddress);
         (uint256 borrowerRiskSlipBalanceBefore, uint256 lendAmount) = repayMaxAndUnwrapSimpleTestSetup(_lendAmount);
@@ -60,5 +60,33 @@ contract RedeemRiskSlipsForTranchesAndUnwrap is RedeemLendSlipsForStablesTestSet
 
         assertTrue(withinTolerance(borrowerUnderlyingBalanceBefore + underlyingAmount, borrowerUnderlyingBalanceAfter, 1));
         assertFalse(underlyingAmount == 0);
+    }
+
+    function testRedeemRiskSlipsForTranchesAndUnwrapWithdrawsCollateralFromBond(uint256 _fuzzPrice, uint256 _lendAmount) public {
+        setupStagingBox(_fuzzPrice);
+        setupTranches(false, s_owner, s_deployedCBBAddress);
+        (uint256 borrowerRiskSlipBalanceBefore, uint256 lendAmount) = repayMaxAndUnwrapSimpleTestSetup(_lendAmount);
+
+        vm.warp(s_deployedConvertibleBondBox.maturityDate() + 1);
+
+        s_deployedConvertibleBondBox.bond().mature();
+
+        uint256 riskTrancheCollateralBalanceBefore = s_collateralToken.balanceOf(address(s_deployedConvertibleBondBox.riskTranche()));
+
+        uint256 riskSlipRedeemAmount = bound(borrowerRiskSlipBalanceBefore, s_deployedConvertibleBondBox.riskRatio(), borrowerRiskSlipBalanceBefore);
+
+        (uint256 underlyingAmount, uint256 buttonAmount) = 
+        IStagingBoxLens(s_stagingBoxLens).viewRedeemRiskSlipsForTranches(s_deployedSB, riskSlipRedeemAmount);
+
+        vm.prank(s_borrower);
+        StagingLoanRouter(s_stagingLoanRouter).redeemRiskSlipsForTranchesAndUnwrap(
+            s_deployedSB, 
+            riskSlipRedeemAmount
+            );
+
+        uint256 riskTrancheCollateralBalanceAfter = s_collateralToken.balanceOf(address(s_deployedConvertibleBondBox.riskTranche()));
+
+        assertTrue(withinTolerance(riskTrancheCollateralBalanceBefore - buttonAmount, riskTrancheCollateralBalanceAfter, 1));
+        assertFalse(buttonAmount == 0);
     }
 }
