011ebe3c02310da83587f6bdd95f134b96636d79
BearMauler <bearmauler88@gmail.com>
2022-07-20 11:58:27 -0500

    wrote another redeemRiskSlipsForTranches test

--- test/stagingLoanRouter/RedeemRiskSlipsForTranchesAndUnwrap.t.sol
+++ test/stagingLoanRouter/RedeemRiskSlipsForTranchesAndUnwrap.t.sol
@@ -33,4 +33,32 @@ contract RedeemRiskSlipsForTranchesAndUnwrap is RedeemLendSlipsForStablesTestSet
 
         assertEq(borrowerRiskSlipBalanceBefore - riskSlipRedeemAmount, borrowerRiskSlipBalanceAfter);
     }
+
+        function testRedeemRiskSlipsForTranchesAndUnwrapSendsUnderlyingToMsgSender(uint256 _fuzzPrice, uint256 _lendAmount) public {
+        setupStagingBox(_fuzzPrice);
+        setupTranches(false, s_owner, s_deployedCBBAddress);
+        (uint256 borrowerRiskSlipBalanceBefore, uint256 lendAmount) = repayMaxAndUnwrapSimpleTestSetup(_lendAmount);
+
+        vm.warp(s_deployedConvertibleBondBox.maturityDate() + 1);
+
+        s_deployedConvertibleBondBox.bond().mature();
+
+        uint256 borrowerUnderlyingBalanceBefore = s_underlying.balanceOf(s_borrower);
+
+        uint256 riskSlipRedeemAmount = bound(borrowerRiskSlipBalanceBefore, s_deployedConvertibleBondBox.riskRatio(), borrowerRiskSlipBalanceBefore);
+
+        (uint256 underlyingAmount, uint256 buttonAmount) = 
+        IStagingBoxLens(s_stagingBoxLens).viewRedeemRiskSlipsForTranches(s_deployedSB, riskSlipRedeemAmount);
+
+        vm.prank(s_borrower);
+        StagingLoanRouter(s_stagingLoanRouter).redeemRiskSlipsForTranchesAndUnwrap(
+            s_deployedSB, 
+            riskSlipRedeemAmount
+            );
+
+        uint256 borrowerUnderlyingBalanceAfter = s_underlying.balanceOf(s_borrower);
+
+        assertTrue(withinTolerance(borrowerUnderlyingBalanceBefore + underlyingAmount, borrowerUnderlyingBalanceAfter, 1));
+        assertFalse(underlyingAmount == 0);
+    }
 }
