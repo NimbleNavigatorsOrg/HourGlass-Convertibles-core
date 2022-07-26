42296cc2f724d51b5c04b3765aee2727c858f928
G-Yes95 <g-yes95@protonmail.com>
2022-07-26 10:29:36 -0700

    added scripts

---
+++ script/KovanAnvil.s.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Script.sol";
+import "../src/contracts/Slip.sol";
+import "../src/contracts/SlipFactory.sol";
+import "../src/contracts/ConvertibleBondBox.sol";
+import "../src/contracts/CBBFactory.sol";
+import "../src/contracts/StagingBox.sol";
+import "../src/contracts/StagingBoxFactory.sol";
+import "../src/contracts/StagingBoxLens.sol";
+import "../src/contracts/StagingLoanRouter.sol";
+import "../test/mocks/MockERC20.sol";
+import "@buttonwood-protocol/tranche/contracts/interfaces/IBondFactory.sol";
+import "@buttonwood-protocol/button-wrappers/contracts/ButtonTokenFactory.sol";
+import "@buttonwood-protocol/button-wrappers/contracts/ButtonToken.sol";
+
+contract KovanAnvil is Script {
+    IBondFactory s_bondFactory =
+        IBondFactory(0x8c0D2e727bE5a421EE9cFf96A3cC4FCaf811424f);
+    ButtonTokenFactory s_buttonTokenFactory =
+        ButtonTokenFactory(0xCCcadb93F162F7516ba7C046f74b628Adf63E6d9);
+    address public s_forthChainlinkOracleAddress =
+        0xEA68d72c6Fe193D74847C5dc537725cd48453f35;
+
+    uint256 public s_penalty = 0;
+    uint256 public s_trancheIndex = 0;
+    uint256 public s_initialPrice = (1e8 * 4) / 5;
+    address public s_weenus = 0xaFF4481D10270F50f203E0763e2597776068CBc5;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        // create mock FORTH token and mint to self
+        MockERC20 anvilForth = new MockERC20("ForgeForth", "fgFORTH");
+
+        // //deploy slips + slip factory
+        Slip deployedSlip = new Slip();
+        SlipFactory slipFactory = new SlipFactory(address(deployedSlip));
+
+        //deploy CBB + CBB factory
+        ConvertibleBondBox deployedCBB = new ConvertibleBondBox();
+        CBBFactory cbbFactory = new CBBFactory(address(deployedCBB));
+
+        //staging box + staging box factory
+        StagingBox deployedStagingBox = new StagingBox();
+        StagingBoxFactory sbFactory = new StagingBoxFactory(
+            address(deployedStagingBox)
+        );
+
+        //deploy staging box router + lens
+        StagingLoanRouter stagingLoanRouter = new StagingLoanRouter();
+        StagingBoxLens stagingBoxLens = new StagingBoxLens();
+
+        //deploy buttonToken
+        ButtonToken buttonForth = ButtonToken(
+            s_buttonTokenFactory.create(
+                address(anvilForth),
+                "button-Forge-Forth",
+                "bFgFORTH",
+                s_forthChainlinkOracleAddress
+            )
+        );
+
+        //deploy buttonForthBond with 20/80 ratio for maturity in 30 days
+        uint256[] memory ratios = new uint256[](2);
+        ratios[0] = 200;
+        ratios[1] = 800;
+
+        address createdBond = s_bondFactory.createBond(
+            address(buttonForth),
+            ratios,
+            block.timestamp + 2592e3
+        );
+
+        //create new SB with CBB
+        address createdSB = sbFactory.createStagingBoxWithCBB(
+            cbbFactory,
+            slipFactory,
+            IBondController(createdBond),
+            s_penalty,
+            s_weenus,
+            s_trancheIndex,
+            s_initialPrice,
+            msg.sender
+        );
+
+        //some test transactions
+
+        // //deposit mockFORTH and send to msg.sender
+        // anvilForth.mint(msg.sender, 15e24);
+
+        vm.stopBroadcast();
+
+        console2.log(address(anvilForth), "anvilForthAddress");
+        console2.log(address(buttonForth), "buttonForth");
+        console2.log(address(deployedSlip), "deployedSlip");
+        console2.log(address(slipFactory), "slipFactory");
+        console2.log(address(cbbFactory), "ConvertiblesFactory");
+        console2.log(address(sbFactory), "StagingBoxFactory");
+        console2.log(address(stagingLoanRouter), "StagingLoanRouter");
+        console2.log(address(stagingBoxLens), "StagingBoxLens");
+        console2.log(createdSB, "createdSB");
+        console2.log(createdBond, "createdBond");
+    }
+}
---
+++ script/KovanBatchDeployer.s.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Script.sol";
+import "../src/contracts/Slip.sol";
+import "../src/contracts/SlipFactory.sol";
+import "../src/contracts/ConvertibleBondBox.sol";
+import "../src/contracts/CBBFactory.sol";
+import "../src/contracts/StagingBox.sol";
+import "../src/contracts/StagingBoxFactory.sol";
+import "../src/contracts/StagingBoxLens.sol";
+import "../src/contracts/StagingLoanRouter.sol";
+import "../test/mocks/MockERC20.sol";
+import "@buttonwood-protocol/tranche/contracts/interfaces/IBondFactory.sol";
+import "@buttonwood-protocol/button-wrappers/contracts/ButtonTokenFactory.sol";
+import "@buttonwood-protocol/button-wrappers/contracts/ButtonToken.sol";
+
+contract KovanSlipDeployer is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        // //deploy slips + slip factory
+        Slip deployedSlip = new Slip();
+        SlipFactory slipFactory = new SlipFactory(address(deployedSlip));
+
+        //deploy CBB + CBB factory
+        ConvertibleBondBox deployedCBB = new ConvertibleBondBox();
+        CBBFactory cbbFactory = new CBBFactory(address(deployedCBB));
+
+        //staging box + staging box factory
+        StagingBox deployedStagingBox = new StagingBox();
+        StagingBoxFactory sbFactory = new StagingBoxFactory(
+            address(deployedStagingBox)
+        );
+
+        //deploy staging box router + lens
+        StagingLoanRouter stagingLoanRouter = new StagingLoanRouter();
+        StagingBoxLens stagingBoxLens = new StagingBoxLens();
+
+        vm.stopBroadcast();
+
+        console2.log(address(deployedSlip), "deployedSlip");
+        console2.log(address(slipFactory), "slipFactory");
+        console2.log(address(cbbFactory), "ConvertiblesFactory");
+        console2.log(address(sbFactory), "StagingBoxFactory");
+        console2.log(address(stagingLoanRouter), "StagingLoanRouter");
+        console2.log(address(stagingBoxLens), "StagingBoxLens");
+    }
+}
